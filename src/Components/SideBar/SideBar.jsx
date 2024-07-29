ee163c112e748734d0ddc166f29c48559633f78d
Al Ifran <ifran15-2915@diu.edu.bd>
2024-07-29 14:59:00 +0600

    updated

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -16,61 +16,61 @@ const data = [
     "link": 'fees_setup'
   },
   {
-    "id": 2,
+    "id": 3,
     "img": "",
     "name": "Installment Setup",
     "link": 'installment_setup'
   },
   {
-    "id": 2,
+    "id": 4,
     "img": "",
     "name": "Create Student Id",
     "link": 'create_student_id'
   },
   {
-    "id": 2,
+    "id": 5,
     "img": "",
     "name": "Student List",
     "link": 'student_list'
   },
   {
-    "id": 2,
+    "id": 6,
     "img": "",
     "name": "Student Status",
     "link": 'student_status'
   },
   {
-    "id": 2,
+    "id": 7,
     "img": "",
     "name": "Total Running Student",
     "link": 'total_running_student'
   },
   {
-    "id": 2,
+    "id": 8,
     "img": "",
     "name": "Total Old Student",
     "link": 'total_old_student'
   },
   {
-    "id": 2,
+    "id": 9,
     "img": "",
     "name": "Create Teacher Id",
     "link": 'create_teacher_id'
   },
   {
-    "id": 2,
+    "id": 10,
     "img": "",
     "name": "Teacher List",
     "link": 'teacher_list'
   },
   {
-    "id": 2,
+    "id": 11,
     "img": "",
     "name": "Search Teacher",
     "link": 'search_teacher'
   },
   {
-    "id": 2,
+    "id": 12,
     "img": "",
     "name": "Money Receipt",
     "link": 'money_receipt'
