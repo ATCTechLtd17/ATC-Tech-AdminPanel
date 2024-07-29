8ff2e3cb7d22e32c33a64de94e5102614ec92011
Al Ifran <ifran15-2915@diu.edu.bd>
2024-07-29 17:14:59 +0600

    updated

--- src/Pages/Academy Pages/CourseSetup.jsx
+++ src/Pages/Academy Pages/CourseSetup.jsx
@@ -6,7 +6,47 @@ const CourseSetup = () => {
     <div>
       <ContainerMain>
         <section>
-          <h1>Course Setup</h1>
+          <div className="flex flex-col gap-1">
+            <label className="font-medium" htmlFor="course_name">
+              Course Name:
+            </label>
+            <input
+              className="input input-bordered input-sm  hover:border-sky-600  "
+              name="branchName"
+              type="text"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label className="font-medium" htmlFor="occupation">
+              Duration:
+            </label>
+            <select className="input input-bordered input-sm  hover:border-sky-600  ">
+              <option>---Select---</option>
+              <option>2 Months</option>
+              <option>3 Months</option>
+              <option>4 Months</option>
+              <option>6 Months</option>
+              <option>8 Months</option>
+            </select>
+          </div>
+
+          <div className="w-full flex justify-center  mt-24 mb-24">
+            <button className="bg-sky-600 hover:bg-teal-700 px-20 py-2 rounded font-medium     text-white">
+              submit
+            </button>
+          </div>
+
+          <div className="md:m-2 xl:m-8">
+            <table className="w-full mb-24">
+              <tr className="grid grid-cols-3   text-xs md:text-base bg-gray-500  py-2 text-white md:grid-cols-9 items-center justify-center gap-1 text-center">
+                <th>SL No.</th>
+                <th className="col-span-2 hidden md:block">Course Name</th>
+                <th className="col-span-2">Duration</th>
+              </tr>
+            </table>
+          </div>
+
         </section>
       </ContainerMain>
 
