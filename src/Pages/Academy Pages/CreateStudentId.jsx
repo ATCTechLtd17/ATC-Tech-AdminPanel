c450a552bf9c7fd3b8b8c2915029d51db5984777
Al Ifran <ifran15-2915@diu.edu.bd>
2024-08-03 19:49:41 +0600

    updated

--- src/Pages/Academy Pages/CreateStudentId.jsx
+++ src/Pages/Academy Pages/CreateStudentId.jsx
@@ -5,7 +5,14 @@ const CreateStudentId = () => {
     <div>
       <ContainerMain>
         <section>
-          <h1>Create Student Id</h1>
+          <div>
+            <img className="h-14 w-22" src="/src/assets/Logo/ATC LOGO3-01.jpg" alt="ATC Tech Ltd Logo" />
+            <p className="font-bold text-3xl">ATC TECH LIMITED</p>
+            <p>ATC ACADEMY: Level-7, Suit-A,BSMHTP, RAJSHAHI</p>
+            <p className="font-bold">Student Admission Form</p>
+            <p>FORMXXIV-I</p>
+
+          </div>
         </section>
       </ContainerMain>
     </div>
