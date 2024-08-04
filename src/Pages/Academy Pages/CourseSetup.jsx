f6819a6dc7f9ce115b9a178c674b2d44d525e450
NashitGit <nazmulnashitofficial@gmail.com>
2024-08-04 12:12:39 +0600

    dinamic dropdown

--- src/Pages/Academy Pages/CourseSetup.jsx
+++ src/Pages/Academy Pages/CourseSetup.jsx
@@ -1,13 +1,20 @@
 
-import { data } from "autoprefixer";
 import SubmitButton from "../../Components/Button/SubmitButton";
 import ContainerMain from "../../Components/Container/ContainerMain";
 import GridContainer from "../../Components/Container/GridContainer";
 import TextInput from "../../Components/Input/TextInput";
+import DropDownInput from "../../Components/Input/DropDownInput";
 
+const dropDowndata = [
+  { value: '2 Months', label: '2 Months' },
+  { value: '3 Months', label: '3 Months' },
+  { value: '4 Months', label: '4 Months' },
+  { value: '6 Months', label: '6 Months' },
+  { value: '8 Months', label: '8 Months' },
+];
 
 const CourseSetup = () => {
-  const data = 'adasdasd'
+
   return (
     <div>
       <ContainerMain>
@@ -25,12 +32,12 @@ const CourseSetup = () => {
             </div>
              */}
             <TextInput name={'courseName'} />
-            <TextInput name={'entryDuration'}/>
-            <TextInput name={'entryFees'}/>
+            <TextInput name={'entryDuration'} />
+            <TextInput name={'entryFees'} />
+            <DropDownInput name={'duration'} options={dropDowndata}  required={true} />
 
 
-
-            <div className="flex flex-col gap-1">
+            {/* <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="occupation">
                 Duration:
               </label>
@@ -42,7 +49,8 @@ const CourseSetup = () => {
                 <option>6 Months</option>
                 <option>8 Months</option>
               </select>
-            </div>
+            </div> */}
+
           </GridContainer>
 
 
